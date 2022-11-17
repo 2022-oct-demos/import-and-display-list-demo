3f4a48eed98ff1d46552890725d5eb8692c9f5be
maddenlockin <maddenlockin@gmail.com>
2022-11-17 10:45:11 -0800

    add test for renderMovie

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,6 @@
 // import functions under test
+import { fetchMovies } from '../fetch-utils.js';
+import { renderMovie } from '../render-utils.js';
 
 const test = QUnit.test;
 
@@ -11,4 +13,16 @@ test('example test...', (expect) => {
 
     // Assert
     expect.deepEqual(actual, expected);
+});
+
+test('test movie render function', async (expect) => {
+    const expected = `<div><h2>Snowmageddon</h2><p>Snowmageddon is a Adventure|Fantasy</p><ul><li>Gunther</li><li>Odette</li><li>Loreen</li></ul></div>`;
+
+    const movies = await fetchMovies();
+    const actual = renderMovie(movies[0]);
+    // console.log('actual', actual);
+    // console.log('actual.outerHTML', actual.outerHTML);
+    // console.log('actual.innerHTML', actual.innerHTML);
+
+    expect.equal(actual.outerHTML, expected);
 });
